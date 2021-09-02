4e5fe482aca38743c49988f651ef435d234147b6
izaak svec <slavi.svec@gmail.com>
2021-09-02 21:12:11 +1000

    Add handleStart component

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+
+import './styles.css'
 
 const Timer = () => {
+  const [seconds, setSeconds] = useState(0)
+
+  let intervalRef = useRef()
+
+  const handleStart = () => {
+    intervalRef.current = setInterval(() => {
+      setSeconds(decreace => decreace - 1)
+    }, 1000)
+  }
   return (
     <>
       <div className="timer-container">
@@ -8,7 +19,8 @@ const Timer = () => {
         <div className="clock-face">
           <span className="timer-minutes">00</span>
           <span className="timer-colon">:</span>
-          <span className="timer-seconds">00</span>
+          <span className="timer-seconds">{seconds}</span>
+          <button onClick={handleStart}>Start</button>
         </div>
       </div>
     </>
